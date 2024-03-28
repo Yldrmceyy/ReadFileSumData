71b411e4684c04d977575c8aecb09ca5d5dd4b5e
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-03-28 20:14:24 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
